9429c3549e213be7883fe51ea4a690adfa12fd3b
Michael Chang <95changmichael13@gmail.com>
2023-06-08 21:27:43 -0400

    clean up

--- app.py
+++ app.py
@@ -5,19 +5,16 @@ import os
 import streamlit as st
 from streamlit_chat import message
 
-from data_driven_characters.character import generate_character_definition
+from data_driven_characters.character import generate_character_definition, Character
 from data_driven_characters.corpus import (
-    get_rolling_summaries,
-    load_docs,
+    generate_rolling_summaries,
+    generate_docs,
 )
 from data_driven_characters.chatbots import (
     SummaryChatBot,
     RetrievalChatBot,
     GenerativeChatBot,
 )
-
-OUTPUT_ROOT = "app/output"
-DATA_ROOT = "app/data"
 
 
 def reset_chat():
@@ -30,17 +27,6 @@ def reset_chat():
 def clear_user_input():
     if "user_input" in st.session_state:
         st.session_state.user_input = ""
-
-
-# Bug: when I change the character name or change the memory type (after a previous session), then I get
-
-# DuplicateWidgetID: There are multiple identical st.streamlit_chat.streamlit_chat widgets with the same generated key.
-
-# When a widget is created, it's assigned an internal key based on its structure. Multiple widgets with an identical structure will result in the same internal key, which causes this error.
-
-# To fix this error, please pass a unique key argument to st.streamlit_chat.streamlit_chat.
-
-# do I get the error if I enter in the same message twice?
 
 
 @st.cache_resource()
@@ -63,62 +49,55 @@ def create_chatbot(character_definition, rolling_summaries, chatbot_type):
 
 
 @st.cache_data(persist="disk")
-def process_corpus(corpus_path, summaries_dir):
+def process_corpus(corpus):
     # load docs
-    docs = load_docs(
-        corpus_path=corpus_path,
+    docs = generate_docs(
+        corpus=corpus,
         chunk_size=2048,
         chunk_overlap=64,
     )
 
     # generate rolling summaries
-    rolling_summaries = get_rolling_summaries(docs=docs, cache_dir=summaries_dir)
+    rolling_summaries = generate_rolling_summaries(docs=docs)
     return rolling_summaries
 
 
+# TODO: this should be a json
 @st.cache_data(persist="disk")
 def get_character_definition(name, rolling_summaries):
     character_definition = generate_character_definition(
         name=name,
         rolling_summaries=rolling_summaries,
     )
-    return character_definition
+    return asdict(character_definition)
 
 
 def main():
-    os.makedirs(OUTPUT_ROOT, exist_ok=True)
-    os.makedirs(DATA_ROOT, exist_ok=True)
-
-    st.title("Data Driven Characters")
+    st.title("Data-Driven Characters")
     st.write("Create your own character chatbots, grounded in existing corpora.")
+    openai_api_key = st.text_input(
+        label="Your OpenAI API KEY",
+        placeholder="Your OpenAI API KEY",
+        type="password",
+    )
 
     with st.sidebar:
         uploaded_file = st.file_uploader("Upload corpus")
         if uploaded_file is not None:
-            # logging
             corpus_name = os.path.splitext(os.path.basename(uploaded_file.name))[0]
-            corpus_path = f"{DATA_ROOT}/{corpus_name}.txt"
-            output_dir = f"{OUTPUT_ROOT}/{corpus_name}"
-            os.makedirs(output_dir, exist_ok=True)
-            summaries_dir = f"{output_dir}/summaries"
-            character_definitions_dir = f"{output_dir}/character_definitions"
-            os.makedirs(character_definitions_dir, exist_ok=True)
 
             # read file
             stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            string_data = stringio.read()
+            corpus = stringio.read()
 
             # scrollable text
             st.markdown(
                 f"""
                 <div style='overflow: auto; height: 300px; border: 1px solid gray; border-radius: 5px; padding: 10px'>
-                    {string_data}</div>
+                    {corpus}</div>
                 """,
                 unsafe_allow_html=True,
             )
-            # save string_data to a file
-            with open(corpus_path, "w") as f:
-                f.write(string_data)
 
             st.divider()
 
@@ -126,6 +105,12 @@ def main():
             character_name = st.text_input(f"Enter a character name from {corpus_name}")
 
             if character_name:
+                if not openai_api_key:
+                    st.error(
+                        "You must enter an API key to use the OpenAI API. Please enter an API key in the sidebar."
+                    )
+                    return
+
                 if (
                     "character_name" in st.session_state
                     and st.session_state["character_name"] != character_name
@@ -135,18 +120,7 @@ def main():
                 st.session_state["character_name"] = character_name
 
                 with st.spinner("Processing corpus (this will take a while)..."):
-                    # # load docs
-                    docs = load_docs(
-                        corpus_path=corpus_path,
-                        chunk_size=2048,
-                        chunk_overlap=64,
-                    )
-
-                    # generate rolling summaries
-                    rolling_summaries = get_rolling_summaries(
-                        docs=docs, cache_dir=summaries_dir
-                    )
-                    # rolling_summaries = process_corpus(corpus_path, summaries_dir)
+                    rolling_summaries = process_corpus(corpus)
 
                 with st.spinner("Generating character definition..."):
                     # get character definition
@@ -154,7 +128,8 @@ def main():
                         name=character_name,
                         rolling_summaries=rolling_summaries,
                     )
-                    print(json.dumps(asdict(character_definition), indent=4))
+
+                    print(json.dumps(character_definition, indent=4))
                     chatbot_type = st.selectbox(
                         "Select a memory type",
                         options=["summary", "retrieval", "generative"],
@@ -171,12 +146,12 @@ def main():
                     st.markdown(
                         f"[Export to character.ai](https://beta.character.ai/editing):"
                     )
-                    st.write(asdict(character_definition))
+                    st.write(character_definition)
 
     if uploaded_file is not None and character_name:
         st.divider()
         chatbot = create_chatbot(
-            character_definition=character_definition,
+            character_definition=Character(**character_definition),
             rolling_summaries=rolling_summaries,
             chatbot_type=chatbot_type,
         )
@@ -186,7 +161,6 @@ def main():
             label=f"Chat with {chatbot.character_definition.name}",
             placeholder=f"Chat with {chatbot.character_definition.name}",
             label_visibility="collapsed",
-            key="user_input",
         )
 
         reset_chatbot = right.button("Reset", on_click=clear_user_input)
@@ -201,12 +175,9 @@ def main():
         for msg in st.session_state.messages:
             message(msg["content"], is_user=msg["role"] == "user")
 
-        # if user_input and not openai_api_key:
-        #     st.info("Please add your OpenAI API key to continue.")
-
         # the new message
-        if user_input:  # and openai_api_key:
-            # openai.api_key = openai_api_key
+        if user_input:
+            st.session_state["user_input"] = user_input
             st.session_state.messages.append({"role": "user", "content": user_input})
             message(user_input, is_user=True)
             with st.spinner(f"{chatbot.character_definition.name} is thinking..."):
