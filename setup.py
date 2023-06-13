8fa8798cd47f24fe129435b2ebe0f6db419e0b2c
mbchang <mbchang@users.noreply.github.com>
2023-06-13 21:53:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ setup(
     name="data_driven_characters",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        'faiss-cpu',
+        'langchain',
+        'loguru',
+        'openai',
+        'streamlit_chat',
+        'tiktoken',
+        'tqdm',
+    ],
 )
-
